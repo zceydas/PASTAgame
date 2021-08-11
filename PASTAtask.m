412ba7b111d92230373983d29d55aac4ed2da7bf
zceydas <25991070+zceydas@users.noreply.github.com>
2021-08-11 16:43:22 -0400

    Added EEG triggers

--- PASTAtask.m
+++ PASTAtask.m
@@ -4,34 +4,36 @@ namestr = st.name;
 directory=fileparts(which([namestr, '.m']));
 cd(directory)
 
-trialdeadline=60; % in seconds (for idea generation)
-
 PsychDefaultSetup(2);
 Screen('Preference', 'SkipSyncTests', 1);
 %PsychDebugWindowConfiguration(0,0.5) % for debugging purposes
 
-subjectId=input('What is subject ID?');
-Session=input('What is study session? Test(1), ReTest(2): '); % this can be 1 or 2 (test and re-test)
+design.trialdeadline=60; % in seconds (for idea generation)
+design.subjectId=input('What is subject ID?');
+design.Session=input('What is study session? Test(1), ReTest(2): '); % this can be 1 or 2 (test and re-test)
+design.runEEG=input('Are you recording EEG? Yes(1), No(0): '); % this can be 1 or 2 (test and re-test)
 
-datafileName = ['ID_' num2str(subjectId) '_SessionNo' num2str(Session) '_Data Folder'];
+datafileName = ['ID_' num2str(design.subjectId) '_SessionNo' num2str(design.Session) '_Data Folder'];
 if ~exist(datafileName, 'dir')
     mkdir(datafileName);
 end
 
+if design.runEEG
+    design.sp           = BioSemiSerialPort();
+end
 
 %Import the options of the excel file
 opts=detectImportOptions('PASTAtrials.xlsx');
 ConditionList=readtable('PASTAtrials.xlsx',opts, 'ReadVariableNames', true);
 
 rng('default')
-rng(subjectId)
+rng(design.subjectId)
 order=shuffle([1:30]); % on each session 15 of them will be presented
-if Session == 1
+if design.Session == 1
     sessionorder=order(1:15);
-elseif Session == 2
+elseif design.Session == 2
     sessionorder=order(16:30);
 end
-
 
 mic_image=imread('mic_image.png');
 baseRect = [0 0 200 200];
@@ -47,28 +49,28 @@ screenNumber = max(screens);
 % Define black, white and grey
 black = BlackIndex(screenNumber);
 white = WhiteIndex(screenNumber);
-grey = white / 2;
+design.grey = white / 2;
 
 % Open an on screen window and color it grey
-[window, windowRect] = PsychImaging('OpenWindow', screenNumber, black);
+[design.window, design.windowRect] = PsychImaging('OpenWindow', screenNumber, black);
 
 % Set the blend funciton for the screen
-Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
+Screen('BlendFunction', design.window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 
 % Get the size of the on screen window in pixels
 % For help see: Screen WindowSize?
-[screenXpixels, screenYpixels] = Screen('WindowSize', window);
+[screenXpixels, design.screenYpixels] = Screen('WindowSize', design.window);
 
 % Get the centre coordinate of the window in pixels
 % For help see: help RectCenter
-[xCenter, yCenter] = RectCenter(windowRect);
-allRects(:, 1) = CenterRectOnPointd(baseRect, xCenter, yCenter);
+[design.xCenter, design.yCenter] = RectCenter(design.windowRect);
+design.allRects(:, 1) = CenterRectOnPointd(baseRect, design.xCenter, design.yCenter);
 numchannels = 1; % mono sound
 % you can CHOOSE your frequency when recording
-freq = 44100;
+design.freq = 44100;
    
 % we also open an audio channel when we're doing recording
-audiochannel = PsychPortAudio('Open', [], 2, [], freq, numchannels);
+audiochannel = PsychPortAudio('Open', [], 2, [], design.freq, numchannels);
 % except we set it to 2 instead of 1
 % 2 is for recording mode
 % 3 is for BOTH
@@ -80,8 +82,8 @@ PsychPortAudio('GetAudioData', audiochannel, 10);
 % if you use MORE than the time you alloted, it starts writing over the
 % start of the sound (like an old VHS tape)
 
-Screen('TextSize', window, 30);
-PASTAInstructions(window,grey);
+Screen('TextSize', design.window, 30);
+PASTAInstructions(design);
 
 Trialtype='Practice'; 
 Index = find(contains(ConditionList.TrialType,Trialtype));
@@ -91,18 +93,18 @@ for t=1:length(Index)
     Ex1=ConditionList.Example1{Index(t)};
     Ex2=ConditionList.Example2{Index(t)};
     Ex3=ConditionList.Example3{Index(t)};
-    fontsize=70;
-    [Results,counter]=PASTAtrialstructure(counter,Results,Session,trialdeadline,subjectId,window,grey,fontsize,screenYpixels,screenXpixels,audiochannel,freq,mic_image,allRects,t,Category,Ex1,Ex2,Ex3,Trialtype);
+    design.fontsize=70;
+    [Results,counter]=PASTAtrialstructure(design,counter,Results,audiochannel,mic_image,t,Category,Ex1,Ex2,Ex3,Trialtype);
 end
 PracticeTable=cell2table(Results,'VariableNames',{'TrialNo' 'Category' 'Example1' 'Example2' 'Example3' 'ReadTime' 'RT' 'Recordtime' 'TaskType'});
-writetable(PracticeTable,['PracticeResults',num2str(Session),'_' 'subject',num2str(subjectId),'_',date,'.xlsx']);
+writetable(PracticeTable,['PracticeResults',num2str(design.Session),'_' 'subject',num2str(design.subjectId),'_',date,'.xlsx']);
 
-Screen('TextSize', window, 30);
-DrawFormattedText(window, sprintf('%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s', ...
+Screen('TextSize', design.window, 30);
+DrawFormattedText(design.window, sprintf('%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s', ...
     'Great! You are now done with practice.', ...
     'If you have any questions, ask the researcher now. ', ...
-    'Otherwise, press a key to start testing.'), 'center', 'center',grey,[100],[],[],[2]);
-Screen('Flip',window); KbStrokeWait;
+    'Otherwise, press a key to start testing.'), 'center', 'center',design.grey,[100],[],[],[2]);
+Screen('Flip',design.window); KbStrokeWait;
 
 Trialtype='Test'; Results={}; counter=1;
 for t=1:length(sessionorder)
@@ -111,28 +113,28 @@ for t=1:length(sessionorder)
     Ex1=ConditionList.Example1{sessionorder(t)};
     Ex2=ConditionList.Example2{sessionorder(t)};
     Ex3=ConditionList.Example3{sessionorder(t)};
-    fontsize=70;
-    [Results,counter]=PASTAtrialstructure(counter,Results,Session,trialdeadline,subjectId,window,grey,fontsize,screenYpixels,screenXpixels,audiochannel,freq,mic_image,allRects,t,Category,Ex1,Ex2,Ex3,Trialtype);
+    design.fontsize=70;
+    [Results,counter]=PASTAtrialstructure(design,counter,Results,audiochannel,mic_image,t,Category,Ex1,Ex2,Ex3,Trialtype);
 end
 TestTable=cell2table(Results,'VariableNames',{'TrialNo' 'Category' 'Example1' 'Example2' 'Example3' 'ReadTime' 'RT' 'Recordtime' 'TaskType'});
-writetable(TestTable,['TestResults',num2str(Session),'_' 'subject',num2str(subjectId),'_',date,'.xlsx']);
+writetable(TestTable,['TestResults',num2str(design.Session),'_' 'subject',num2str(design.subjectId),'_',date,'.xlsx']);
 
 PsychPortAudio('Close', audiochannel);
 
-files = dir(['subject' num2str(subjectId) '*.wav']);
+files = dir(['subject' num2str(design.subjectId) '*.wav']);
 for f=1:length(files)
     movefile(fullfile(files(f).folder,files(f).name), datafileName)
 end
-tablefiles = dir(['*_subject' num2str(subjectId) '*.xlsx']);
+tablefiles = dir(['*_subject' num2str(design.subjectId) '*.xlsx']);
 for f=1:length(tablefiles)
     movefile(fullfile(tablefiles(f).folder,tablefiles(f).name), datafileName)
 end
 
-Screen('TextSize', window, 30);
-DrawFormattedText(window, sprintf('%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s', ...
+Screen('TextSize', design.window, 30);
+DrawFormattedText(design.window, sprintf('%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s', ...
     'Great! You finished the game!', ...
-    'Thank you for your participation!'), 'center', 'center',grey,[100],[],[],[2]);
-Screen('Flip',window); KbStrokeWait;
+    'Thank you for your participation!'), 'center', 'center',design.grey,[100],[],[],[2]);
+Screen('Flip',design.window); KbStrokeWait;
 
 % Clear the screen
 sca;
--- PASTAtrialstructure.m
+++ PASTAtrialstructure.m
@@ -1,59 +1,64 @@
-function [Results,counter]=PASTAtrialstructure(counter,Results,Session,trialdeadline,subjectId,window,grey,fontsize,screenYpixels,screenXpixels,audiochannel,freq,mic_image,allRects,t,Category,Ex1,Ex2,Ex3,Trialtype)
+function [Results,counter]=PASTAtrialstructure(design,counter,Results,audiochannel,mic_image,t,Category,Ex1,Ex2,Ex3,Trialtype)
 
 ideacount=0;
 RT=0;
 % Draw text in the upper portion of the screen with the default font in red
-Screen('TextSize', window, fontsize);
-DrawFormattedText(window, Category, 'center',...
-    screenYpixels * 0.40 , [1 0 0]);
+Screen('TextSize', design.window, design.fontsize);
+DrawFormattedText(design.window, Category, 'center',...
+    design.screenYpixels * 0.40 , [1 0 0]);
 
 
 % Draw text in the bottom of the screen in Times in blue
-Screen('TextSize', window, fontsize);
-Screen('TextFont', window, 'Times');
-DrawFormattedText(window, Ex1, 'center',...
-    screenYpixels * 0.55, [0 0 1]);
+Screen('TextSize', design.window, design.fontsize);
+Screen('TextFont', design.window, 'Times');
+DrawFormattedText(design.window, Ex1, 'center',...
+    design.screenYpixels * 0.55, [0 0 1]);
 
 % Draw text in the bottom of the screen in Times in blue
-Screen('TextSize', window, fontsize);
-Screen('TextFont', window, 'Times');
-DrawFormattedText(window, Ex2, 'center',...
-    screenYpixels * 0.65, [0 0 1]);
+Screen('TextSize', design.window, design.fontsize);
+Screen('TextFont', design.window, 'Times');
+DrawFormattedText(design.window, Ex2, 'center',...
+    design.screenYpixels * 0.65, [0 0 1]);
 
 % Draw text in the bottom of the screen in Times in blue
-Screen('TextSize', window, fontsize);
-Screen('TextFont', window, 'Times');
-DrawFormattedText(window, Ex3, 'center',...
-    screenYpixels * 0.75, [0 0 1]);
+Screen('TextSize', design.window, design.fontsize);
+Screen('TextFont', design.window, 'Times');
+DrawFormattedText(design.window, Ex3, 'center',...
+    design.screenYpixels * 0.75, [0 0 1]);
 
 if contains(Trialtype,'Practice')
-    Screen('TextSize', window, 35);
-    Screen('TextFont', window, 'Times');
-    DrawFormattedText(window, '(Once you are done reading, press the space bar)', 'center',...
-        screenYpixels * 0.90, grey);
+    Screen('TextSize', design.window, 35);
+    Screen('TextFont', design.window, 'Times');
+    DrawFormattedText(design.window, '(Once you are done reading, press the space bar)', 'center',...
+        design.screenYpixels * 0.90, design.grey);
 end
 
 % Flip to the screen
-Screen('Flip', window); ReadStart=GetSecs; WaitSecs(0.2);
+Screen('Flip', design.window); ReadStart=GetSecs; WaitSecs(0.2);
 KbStrokeWait; Readit=GetSecs; ReadTime=Readit-ReadStart;
 RecordTime=0; Recordall=0;
 
 while 1
+    
+    if design.runEEG
+        design.sp.sendTrigger(counter)
+    end
+    
     % idea generation
-    Screen('TextSize', window, fontsize);
-    Screen('TextFont', window, 'Times');
-    DrawFormattedText(window, '+', 'center',...
-        screenYpixels * 0.55, grey);
+    Screen('TextSize', design.window, design.fontsize);
+    Screen('TextFont', design.window, 'Times');
+    DrawFormattedText(design.window, '+', 'center',...
+        design.screenYpixels * 0.55, design.grey);
     
     if contains(Trialtype,'Practice')
-        Screen('TextSize', window, 35);
-        Screen('TextFont', window, 'Times');
-        DrawFormattedText(window, '(Now try to come up with a new name and press the space bar when ready to say it out loud.)', 'center',...
-            screenYpixels * 0.90, grey);
+        Screen('TextSize', design.window, 35);
+        Screen('TextFont', design.window, 'Times');
+        DrawFormattedText(design.window, '(Now try to come up with a new name and press the space bar when ready to say it out loud.)', 'center',...
+            design.screenYpixels * 0.90, design.grey);
     end
-    Screen('Flip', window);
+    Screen('Flip', design.window);
     [keyIsDown,TimeStamp,keyCode] = KbCheck;
-    if  ((GetSecs-Readit) - Recordall) > trialdeadline
+    if  ((GetSecs-Readit) - Recordall) > design.trialdeadline
         if ideacount == 0
             RT=GetSecs-Readit; % idea generation RT since the beginning of the first idea generation screen
         else
@@ -82,17 +87,17 @@ while 1
         end
         
         % voice recording
-        Screen(window,'PutImage',mic_image,allRects);
+        Screen(design.window,'PutImage',mic_image,design.allRects);
         if contains(Trialtype,'Practice')
-            Screen('TextSize', window, 35);
-            Screen('TextFont', window, 'Times');
-            DrawFormattedText(window, '(Now say the name out loud as we record your voice.', 'center',...
-                screenYpixels * 0.85, grey);
-            DrawFormattedText(window, 'When ready to come up with another new name, press space bar again.)', 'center',...
-                screenYpixels * 0.90, grey);
+            Screen('TextSize', design.window, 35);
+            Screen('TextFont', design.window, 'Times');
+            DrawFormattedText(design.window, '(Now say the name out loud as we record your voice.', 'center',...
+                design.screenYpixels * 0.85, design.grey);
+            DrawFormattedText(design.window, 'When ready to come up with another new name, press space bar again.)', 'center',...
+                design.screenYpixels * 0.90, design.grey);
         end
         
-        Screen('Flip', window); RecordStart=GetSecs;
+        Screen('Flip', design.window); RecordStart=GetSecs;
         ideacount=ideacount+1;
         
         PsychPortAudio('Start',audiochannel,1);
@@ -111,10 +116,10 @@ while 1
         % right now this is just a matrix in MATLAB.  we need to save it to a file
         % on our hard drive
         if ~isempty(recordedaudio)
-            filename = ['subject' num2str(subjectId) '_Session' num2str(Session) '_' Trialtype '_trial' num2str(t) '_response' num2str(ideacount) '.wav']; % in a real experiment, you'd want to have the filename
+            filename = ['subject' num2str(design.subjectId) '_Session' num2str(design.Session) '_' Trialtype '_trial' num2str(t) '_response' num2str(ideacount) '.wav']; % in a real experiment, you'd want to have the filename
             % be based on the current subject & trial
             % no.
-            audiowrite(filename,recordedaudio,freq);
+            audiowrite(filename,recordedaudio,design.freq);
         end
         
         
